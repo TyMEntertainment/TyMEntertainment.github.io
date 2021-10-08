fb4d604f1cfe809e6e7d053bbe1b30ac29c365f8
TyMEntertainment <TyMEntertainment2021@gmail.com>
2021-10-07 23:07:27 -0500

    v3

--- scripts.js
+++ scripts.js
@@ -1,1 +1,28 @@
 /*Zona de variables*/
+
+boton_menu = document.getElementById("icon-menu");
+menu = document.getElementById("menu");
+header = document.getElementById("header");
+fondo = document.getElementById("fondo");
+
+
+/*Variables con funcion*/
+
+document.getElementById("icon-menu").addEventListener("click", mostrar_menu);
+document.getElementById("fondo").addEventListener("click", ocultar_menu);
+
+/*Funciones*/
+
+function mostrar_menu() {
+
+    menu.style.width = "300px";
+    fondo.style.display = "block";
+
+}
+
+function ocultar_menu() {
+
+    menu.style.width = "0px";
+    fondo.style.display = "none";
+
+}
